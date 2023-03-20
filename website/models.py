54cd4fa8b139180063035d4e1096bbb169179d21
zuriBarnes <zurikiser@gmail.com>
2023-03-20 17:55:34 -0400

    model edits manager and team

--- website/models.py
+++ website/models.py
@@ -67,9 +67,8 @@ class Manager(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     email = models.EmailField()
-    team = models.ForeignKey('Team', default='not assigned', on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} | {self.team}"
+        return f"{self.first_name} {self.last_name}"
 
 
