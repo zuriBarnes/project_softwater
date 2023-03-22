6c69b3b388a860ff129cfc84eadb0249369f00ac
zuriBarnes <zurikiser@gmail.com>
2023-03-22 18:31:23 -0400

    created age group constant variable for testing

--- website/models.py
+++ website/models.py
@@ -1,5 +1,32 @@
 from django.db import models
 
+# Age groups
+U18 = 'U18'
+U17 = 'U17'
+U16 = 'U16'
+U15 = 'U15'
+U14 = 'U14'
+U13 = 'U14'
+U12 = 'U12'
+U11 = 'U11'
+U10 = 'U10'
+U9 = 'U9'
+U8 = 'U8'
+    
+AGE_GROUP_CHOICES = [
+    (U18, 'U18'),
+    (U17, 'U17'), 
+    (U16, 'U16'),
+    (U15, 'U15'),
+    (U14, 'U14'),
+    (U13, 'U13'),
+    (U12, 'U12'),
+    (U11, 'U11'), 
+    (U10, 'U10'), 
+    (U9, 'U9'),
+    (U8, 'U8'),
+]
+
 # Create your models here.
 
 class Leader(models.Model):
@@ -8,6 +35,7 @@ class Leader(models.Model):
     email = models.EmailField()
     role = models.CharField(max_length=40)
     bio = models.TextField(default="bio needed")
+    
 
     def __str__(self):
         return f"{self.first_name}, {self.last_name}"
@@ -23,31 +51,7 @@ class Coach(models.Model):
         return f"{self.first_name} {self.last_name} | {self.role}, {self.team}"
 
 class Team(models.Model):
-    # Age groups
-    U18 = 'U18'
-    U17 = 'U17'
-    U16 = 'U16'
-    U15 = 'U15'
-    U14 = 'U14'
-    U13 = 'U14'
-    U12 = 'U12'
-    U11 = 'U11'
-    U10 = 'U10'
-    U9 = 'U9'
-    U8 = 'U8'
-    
-    AGE_GROUP_CHOICES = [(U18, 'U18'),
-                         (U17, 'U17'), 
-                         (U16, 'U16'),
-                         (U15, 'U15'),
-                         (U14, 'U14'),
-                         (U13, 'U13'),
-                         (U12, 'U12'),
-                         (U11, 'U11'), 
-                         (U10, 'U10'), 
-                         (U9, 'U9'),
-                         (U8, 'U8'),
-                         ]
+
     age_group = models.CharField(max_length=3, choices=AGE_GROUP_CHOICES)
     GIRLS = 'Girls'
     BOYS = 'Boys'
@@ -62,11 +66,11 @@ class Team(models.Model):
     def __str__(self):
         return f"{self.age_group} {self.side} | {self.level}"
     
-
 class Manager(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     email = models.EmailField()
+    team = models.CharField(max_length=5, choices=AGE_GROUP_CHOICES)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
